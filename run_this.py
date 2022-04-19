060ae6ce3de133a3f424efdaee5f5fa98bba340b
Aragath <54591654+Aragath@users.noreply.github.com>
2022-04-19 13:54:06 +0800

    :construction: 4/19: finished, deleted str(), render and changed the class name
    changed the Maze class to Board
    deleted the str() used as str(observation), b/c pacman observation is 2D

---
+++ run_this.py
@@ -0,0 +1,38 @@
+from board_env import Board
+from RL_brain import QLearningTable
+
+
+def update():
+    for episode in range(100):
+        # initial observation
+        observation = env.reset()
+
+        while True:
+
+            # RL choose action based on observation
+            action = RL.choose_action(observation)
+
+            # RL take action and get next observation and reward
+            observation_, reward, done = env.step(action)
+
+            # RL learn from this transition
+            RL.learn(observation, action, reward, observation_)
+
+            # swap observation
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                break
+
+    # end of game
+    print('game over')
+    env.destroy()
+
+if __name__ == "__main__":
+
+    # initial env
+    env = Board()
+
+    # initial Q table
+    RL = QLearningTable(actions=list(range(env.n_actions)))
